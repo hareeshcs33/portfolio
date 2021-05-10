cea322fce33c24fde18c163b284a5cbb31af488f
hareeshcs33 <hareeshcs33@gmail.com>
2021-05-10 13:38:28 +0530

    portfolio ui update

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('portfolio');
+
+const projectSubTitle = document.querySelector('.project-sub-title');
+
+const projectDescription = projectSubTitle.innerText;
+
+console.log(projectDescription);
+
+
+// if(projectDescription.length > 45){
+//     this.projectDescription = this.projectDescription + '...';
+// }
+// console.log(projectDescription);
